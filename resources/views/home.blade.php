e0ef35b84baf2c8673825908356492e6df65219c
ShashiBalu <shashibalu8@gmail.com>
2023-09-01 08:48:48 +0530

    images and css syntax

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crystalline</title>
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <img src="{{ asset('assets/images/logo (1).svg') }}" alt="Crystalline Logo" id="logo">
+
+        <h1>Crystalline</h1>
+    </header>
+</body>
+
+</html>
